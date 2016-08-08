3d9f45db2e26b670022b84c5225542fd5813ae65
TuringMacLee <turingmac@hotmail.com>
2016-08-08 15:53:42 +0900

    Ex1 required

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,7 +16,15 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+tmp1 = 0;
+tmp2 = 0;
+for i = 1:m
+  tmp1 = tmp1 + (theta' * X(i,:)' - y(i));
+  tmp2 = tmp2 + (theta' * X(i,:)' - y(i)) * X(i,2);
+end
 
+theta(1) = theta(1) - (alpha / m) * tmp1;
+theta(2) = theta(2) - (alpha / m) * tmp2;
 
 
 
